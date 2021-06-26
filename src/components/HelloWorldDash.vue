793cb82307b8c8c50d1df94e6d3173985d8994d7
Piotr Marecki <pm@aimachina.io>
2021-06-26 11:35:01 +0200

    postowanie cen

--- src/components/HelloWorldDash.vue
+++ src/components/HelloWorldDash.vue
@@ -14,21 +14,32 @@
     <input type="number" v-model="state.price">
   </p>
   <p>
+    <select>
+      <option disabled value="">Please select one</option>
+      <option v-for="l in state.locations" @click="selectLocation(l)">{{ l.nazwa }}, {{ l.ulica }} {{ l.numer }}
+      </option>
+    </select>
+    <span>Selected: {{ state.loc }}</span>
+  </p>
+  <p>
     Akcje:
     <button type="button" class="ourbutton" @click="foo(state.count)">Alert</button>
-    <button type="button" class="ourbutton" @click="callApi()"> Sprawdź ceny</button>
+    <button type="button" class="ourbutton" @click="getPrices()"> Sprawdź ceny</button>
     <button type="button" class="ourbutton" @click="getLocation()"> Sprawdź pozycję</button>
+    <button type="button" class="ourbutton" @click="postPrice()"> Zapostuj cenę</button>
   </p>
   <p>
     Widoczna podana cena: {{ state.price }}
     <br>
-    <span v-if="state.longitude">Pozycja: x={{state.longitude}}&y={{state.latitude}} </span>
+    <span v-if="state.longitude">Pozycja: x={{ state.longitude }}&y={{ state.latitude }} </span>
   </p>
   <div>
     <strong>Z naszej bazy...</strong>
     <ul>
       <li v-for="p in state.prices">
-        {{ p.cenakg }}
+        {{ p.cenakg }}, ({{ p.lokalizacjaid }}): {{ state.locations[p.lokalizacjaid].nazwa }}
+        {{ state.locations[p.lokalizacjaid].ulica }}
+        {{ state.locations[p.lokalizacjaid].numer }}
       </li>
     </ul>
   </div>
@@ -48,7 +59,15 @@ defineProps({
 })
 
 //Dane obecnego komponentu
-const state = reactive({count: 10, price: 8.50, prices: []})
+const state = reactive({
+  count: 10,
+  price: 8.50,
+  prices: [],
+  locations: {},
+  longitude: 0,
+  latitude: 0,
+  loc: ""
+})
 
 
 //Metody/funkcje których będziemy używali w komponencie
@@ -56,10 +75,28 @@ let foo = (e) => {
   alert(`Msg: ${e}`);
 }
 
+let selectLocation = (l) => {
+  state.loc = l.lokalizacjaid;
+}
+
+let postPrice = async () => {
+  const cena = state.price;
+  const lokalizacjaid = state.loc;
+  const response = await fetch(`http://10.10.0.200:3030/cena/add?lokalizacjaid=${lokalizacjaid}&cenakg=${cena}`);
+  alert('Cena została zapostowana, dzięki!')
+}
+
 //Przykład działającego zapytania http
-let callApi = async () => {
+let getPrices = async () => {
   // const response = await fetch("https://jsonplaceholder.typicode.com/todos");
   const response = await fetch("http://10.10.0.200:3030/ceny");
+  const rLocations = await fetch("http://10.10.0.200:3030/lokalizacje");
+  const ll = await rLocations.json();
+  for (let l of ll) {
+    state.locations[l.lokalizacjaid] = l;
+  }
+  console.log(JSON.stringify(state.locations));
+
   state.prices = await response.json();
 }
 
@@ -77,7 +114,6 @@ const getLocation = () => {
     console.log("Geolocation is not supported by this browser.");
   }
 }
-
 
 
 </script>
